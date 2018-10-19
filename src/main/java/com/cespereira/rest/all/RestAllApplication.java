0653bdf9b8019f0fbdddce2c465edf18f0ca11b9
cespereira <5758309+cespereira@users.noreply.github.com>
2018-10-18 21:05:03 -0300

    first commit

---
+++ src/main/java/com/cespereira/rest/all/RestAllApplication.java
@@ -0,0 +1,12 @@
+package com.cespereira.rest.all;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestAllApplication.class, args);
+    }
+}
---
+++ src/test/java/com/cespereira/rest/all/RestAllApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cespereira.rest.all;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestAllApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
